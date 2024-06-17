69632c9c868b6222f8485dfbcf5f1511c532167e
vsaravind01 <vsaravind01@gmail.com>
2024-06-17 21:05:04 +0530

    update start_telegram_server.sh with placeholder environment variables

--- scripts/start_telegram_server.sh
+++ scripts/start_telegram_server.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+export QDRANT_API_KEY=<YOUR_QDRANT_API_KEY>
+export QDRANT_URL=<YOUR_QDRANT_URL>
+export COHERE_API_KEY=<YOUR_COHERE_API_KEY>
+export TELEGRAM_API_KEY=<YOUR_TELEGRAM_API_KEY>
+
 echo "Starting telegram server"
 cd docker/bot || exit
 sudo docker-compose up
